2d0a8152878b01232141d91b430e459e56be556f
James Alhassan <jamesalhassan052@gmail.com>
2022-06-03 01:19:18 +0100

    Add files via upload
    Smooth Scroll

--- app.js
+++ app.js
@@ -49,3 +49,32 @@ window.addEventListener("scroll", function () {
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+
+    const element = document.getElementById(id);
+    // calculating height
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
